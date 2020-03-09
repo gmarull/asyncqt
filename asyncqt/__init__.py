31367e85491d8f20de63529c13965abf6d8b12b2
TheGreatCabbage <20987172+TheGreatCabbage@users.noreply.github.com>
2020-03-09 09:29:08 +0000

    Fix asyncSlot not returning future (#16)
    Co-authored-by: selplacei <35859522+selplacei@users.noreply.github.com>

--- asyncqt/__init__.py
+++ asyncqt/__init__.py
@@ -648,6 +648,6 @@ def asyncSlot(*args):
         @Slot(*args)
         @functools.wraps(fn)
         def wrapper(*args, **kwargs):
-            asyncio.ensure_future(fn(*args, **kwargs))
+            return asyncio.ensure_future(fn(*args, **kwargs))
         return wrapper
     return outer_decorator
